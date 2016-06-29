890b98edb6b6cf182d23ad59202cb8f0774fd78a
zsenseless <zsanter@gmail.com>
2016-06-28 21:20:50 -0400

    Modify utvpiInterpreter
    Move everything that shouldn't be in the header file into the source
    file. I'm curious if C can tolerate me having the struct Parser
    definition in the source file, as long as typedef struct Parser Parser
    is in the header file. It isn't used anywhere else and is just passed
    into initializeSystem and addEdge so it can be passed to parseError,
    which itself is defined in utvpiInterpreter.

--- UTVPIinterpreter.c
+++ UTVPIinterpreter.c
@@ -1,4 +1,63 @@
+#include <stdio.h>
+#include <ctype.h>
 #include "utvpiInterpreter.h"
+
+typedef enum TokenType{
+  SIGN,
+  SIGNED_VARIABLE,
+  UNSIGNED_VARIABLE,
+  LESS_THAN_OR_EQUAL,
+  INTEGER,
+  NEWLINE,
+  VARIABLES_KEYWORD,
+  END_OF_FILE,
+  UNDEFINED,
+} TokenType;
+
+typedef enum BlockCommentState {
+  NOTHING,
+  STAR,
+  SLASH,
+} BlockCommentState;
+
+struct Parser {
+  FILE * constraintFile;
+  void * object;
+  void (* initializeSystem)(void * object, int n, Parser * parser);
+  void (* addEdge)(void * object, Constraint * constraint, Parser * parser);
+  int systemMaxIndex;
+  long int fileLineOffset;
+  int line;
+  int column;
+  bool inMiddleOfBlockComment;
+  bool completedSuccessfully;
+};
+
+typedef struct Token {
+  TokenType type;
+  ConstraintSign sign;
+  int integerComponent;
+} Token;
+
+bool parseVertexCount(Parser * parser);
+void parseConstraints(Parser * parser);
+void afterInitialSign(Parser * parser, Constraint * constraint);
+void afterFirstVariable(Parser * parser, Constraint * constraint);
+void secondVariable(Parser * parser, Constraint * constraint);
+void lessThanOrEqual(Parser * parser, Constraint * constraint);
+void weight(Parser * parser, Constraint * constraint);
+void newLine(Parser * parser, Constraint * constraint);
+bool variableIndexWithinBounds(Parser * parser, Token * token);
+bool variableIndecesDiffer(Parser * parser, Constraint * constraint);
+void newLineAdjustment(Parser * parser);
+Token getToken(Parser * parser);
+void afterSign(Parser * parser, Token * token);
+void afterX(Parser * parser, Token * token);
+void variablesKeyword(Parser * parser, Token * token);
+void lessThanOrEqualToken(Parser * parser, Token * token);
+void continuingInteger(Parser * parser, Token * token);
+int myfgetc(Parser * parser);
+void myungetc(int character, Parser * parser);
 
 bool parseFile(FILE * constraintFile,
         void * object,
--- UTVPIinterpreter.h
+++ UTVPIinterpreter.h
@@ -1,74 +1,16 @@
 #ifndef _UTVPI_INTERPRETER_H
 #define _UTVPI_INTERPRETER_H
 
-#include <stdio.h>
 #include <stdbool.h>
-#include <ctype.h>
 #include "constraint.h"
 
-typedef enum TokenType{
-  SIGN,
-  SIGNED_VARIABLE,
-  UNSIGNED_VARIABLE,
-  LESS_THAN_OR_EQUAL,
-  INTEGER,
-  NEWLINE,
-  VARIABLES_KEYWORD,
-  END_OF_FILE,
-  UNDEFINED,
-} TokenType;
-
-typedef enum BlockCommentState {
-  NOTHING,
-  STAR,
-  SLASH,
-} BlockCommentState;
-
 typedef struct Parser Parser;
-typedef struct Token Token;
-
-struct Parser {
-  FILE * constraintFile;
-  void * object;
-  void (* initializeSystem)(void * object, int n, Parser * parser);
-  void (* addEdge)(void * object, Constraint * constraint, Parser * parser);
-  int systemMaxIndex;
-  long int fileLineOffset;
-  int line;
-  int column;
-  bool inMiddleOfBlockComment;
-  bool completedSuccessfully;
-};
-
-struct Token {
-  TokenType type;
-  ConstraintSign sign;
-  int integerComponent;
-};
 
 bool parseFile(FILE * constraintFile,
     void * object,
     void (* initializeSystem)(void * object, int n, Parser * parser), 
     void (* addEdge)(void * object, Constraint * constraint, Parser * parser));
-bool parseVertexCount(Parser * parser);
-void parseConstraints(Parser * parser);
-void afterInitialSign(Parser * parser, Constraint * constraint);
-void afterFirstVariable(Parser * parser, Constraint * constraint);
-void secondVariable(Parser * parser, Constraint * constraint);
-void lessThanOrEqual(Parser * parser, Constraint * constraint);
-void weight(Parser * parser, Constraint * constraint);
-void newLine(Parser * parser, Constraint * constraint);
-bool variableIndexWithinBounds(Parser * parser, Token * token);
-bool variableIndecesDiffer(Parser * parser, Constraint * constraint);
-void newLineAdjustment(Parser * parser);
-Token getToken(Parser * parser);
-void afterSign(Parser * parser, Token * token);
-void afterX(Parser * parser, Token * token);
-void variablesKeyword(Parser * parser, Token * token);
-void lessThanOrEqualToken(Parser * parser, Token * token);
-void continuingInteger(Parser * parser, Token * token);
-int myfgetc(Parser * parser);
-void myungetc(int character, Parser * parser);
+
 void parseError(Parser * parser, const char * message);
 
 #endif
