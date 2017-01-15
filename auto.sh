59b554a444f63165f617a61badb9149593b37f1f
Zachary Santer <zsanter@gmail.com>
2017-01-15 16:12:02 -0500

    Modify header line of the csv file to relact changed measurement

--- auto.sh
+++ auto.sh
@@ -2,28 +2,28 @@
 
 # ./auto.sh [program] [modify:"variables"|"constraints"] [feasibility:"f0"|"f1"|"f2"]
 #
-# This script profiles the input program with sets of input systems that either modulate the number of variables, keeping the 
+# This script profiles the input program with sets of input systems that either modulate the number of variables, keeping the
 # number of constraints constant; or modulate the number of constraints, keeping the number of variables constant. The feasibility
-# input determines the set of input systems, based on the feasibility setting used to generate these systems, which does not 
+# input determines the set of input systems, based on the feasibility setting used to generate these systems, which does not
 # correlate directly to the similarly-named "f" output that enters the empirical csv file.
 #
-# f0 input systems were generated with no guarantee of feasibility; none of the f0 input systems are linearly feasible. f1 input 
+# f0 input systems were generated with no guarantee of feasibility; none of the f0 input systems are linearly feasible. f1 input
 # systems were generated with a guarantee of at least linear feasibility; one of these turned out to not be linearly feasible and
-# about half of them are also integrally feasible. f2 input systems were generated with a guarantee of integral feasibility; one 
+# about half of them are also integrally feasible. f2 input systems were generated with a guarantee of integral feasibility; one
 # of these also turned out to not be linearly feasible, though the rest are integrally feasible.
 #
-# In the empirical csv file, an f value of 0 indicates a linearly infeasible system; an f value of 1 indicates a system that is 
+# In the empirical csv file, an f value of 0 indicates a linearly infeasible system; an f value of 1 indicates a system that is
 # linearly feasible, but not integrally feasible; and an f value of 2 indicates an integrally feasible system.
 #
-# A full profile of an input program would be completed by running this script with every combination of modify and feasibility. 
-# In some cases, these runs will each individually require a large amount of time, and/or a large amount of memory. This script 
+# A full profile of an input program would be completed by running this script with every combination of modify and feasibility.
+# In some cases, these runs will each individually require a large amount of time, and/or a large amount of memory. This script
 # was originally designed around being run in a screen session and will regularly print status information to the command line if
 # run as a separate process with the ampersand.
 #
-# Any program profiled with this script must print feasibility and timing information to stdout or stderr, with commas delimiting 
+# Any program profiled with this script must print feasibility and timing information to stdout or stderr, with commas delimiting
 # separate values
 #
-# This script requires that all of these input files exist within an "input/" directory, and that "output/" and "empirical/" 
+# This script requires that all of these input files exist within an "input/" directory, and that "output/" and "empirical/"
 # directories also exist within the current working directory.
 
 program="${1}"
@@ -56,10 +56,10 @@ date=`date +%Y-%m-%d`
 csvFile="empirical/${date}_${programNoExt}_Stuckey_${modify}_${f}.csv"
 
 # For use with non-CO* implementations:
-echo "input file,variables,constraints,f,setup,linear,integer,cleanup,total,user,system,maximum resident set size (kB)" > "${csvFile}"
+#echo "input file,variables,constraints,f,setup,linear,integer,cleanup,algorithm,user,system,maximum resident set size (kB)" > "${csvFile}"
 
 # For use with CO* implementations:
-#echo "input file,variables,constraints,f,false positives,main loop iterations,negative cycle length,setup,linear,integer,cleanup,total,user,system,maximum resident set size (kB)" > "${csvFile}"
+echo "input file,variables,constraints,f,false positives,main loop iterations,negative cycle length,setup,linear,integer,cleanup,algorithm,user,system,maximum resident set size (kB)" > "${csvFile}"
 
 for variables in "${variablesArray[@]}" ; do
   for constraints in "${constraintsArray[@]}" ; do
@@ -69,7 +69,7 @@ for variables in "${variablesArray[@]}" ; do
         echo "${programNoExt} ${modify} ${f} processing ${inputFile}"
         outputFile="output/${date}_${programNoExt}_test_n${variables}_m${constraints}_${f}_no${number}_out.txt"
         echo -n "${inputFile},${variables},${constraints}," >> "${csvFile}"
-        /usr/bin/time -f "%U,%S,%M" ./${program} "${inputFile}" "${outputFile}" &>> "${csvFile}" 
+        /usr/bin/time -f "%U,%S,%M" ./${program} "${inputFile}" "${outputFile}" &>> "${csvFile}"
       fi
     done
   done
