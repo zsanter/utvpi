a068db45850c182cb866c402a27a3b3911b9b884
Zachary Santer <zsanter@gmail.com>
2017-01-15 13:52:39 -0500

    Populated delay.h
    hurr durr

--- delay.c
+++ delay.c
@@ -1,7 +1,5 @@
 /**/
 
-#include <stdio.h>
-#include <stdbool.h>
 #include "utvpiInterpreter.h"
 
 typedef struct Delayer {
--- delay.h
+++ delay.h
@@ -1,1 +1,7 @@
-/**/
+#include <stdio.h>
+#include <stdbool.h>
+
+bool parseFileDelayedSysGen(FILE * constraintFile,
+    void * object,
+    void (* initializeSystem)(void * object, int n, Parser * parser),
+    void (* addConstraint)(void * object, Constraint * constraint, Parser * parser));
